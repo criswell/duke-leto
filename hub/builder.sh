3063d68c31273cd2ae4313898ea08d430f978daf
Sam Hart <shart@doximity.com>
2021-04-09 14:01:19 -0400

    start the hub

---
+++ hub/builder.sh
@@ -0,0 +1,15 @@
+# Builder script for the basic web server
+
+image=lighttpd-core-site
+
+# Start by removing the previous image
+buildah rmi $image
+
+httpd_cont=$(buildah from docker.io/sebp/lighttpd:latest)
+
+buildah copy $httpd_cont data/www/index.html /var/www/localhost/htdocs/.
+
+buildah config --entrypoint "/usr/local/bin/start.sh"
+
+buildah commit $httpd_cont $image
+
