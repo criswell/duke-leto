ad7cb57062b4c1f462243e307d756460eb02e810
Sam Hart <shart@doximity.com>
2021-04-09 14:10:49 -0400

    missing container, add port

--- hub/builder.sh
+++ hub/builder.sh
@@ -9,7 +9,8 @@ httpd_cont=$(buildah from docker.io/sebp/lighttpd:latest)
 
 buildah copy $httpd_cont data/www/index.html /var/www/localhost/htdocs/.
 
-buildah config --entrypoint "/usr/local/bin/start.sh"
+buildah config --entrypoint "/usr/local/bin/start.sh" $httpd_cont
+buildah config --port 80 $httpd_cont
 
 buildah commit $httpd_cont $image
 
