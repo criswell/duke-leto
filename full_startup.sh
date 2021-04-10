c0a24a6d0e52b175a8827079c1736cd61264f768
Sam Hart <hartsn@gmail.com>
2021-04-10 15:14:23 -0400

    pushd?

--- full_startup.sh
+++ full_startup.sh
@@ -3,18 +3,15 @@
 # This is a rather brute force script... I really should refine it later
 # Will kill everything, rebuild, and restart
 
-PWD=$(pwd)
-PWD=${PWD%/}
-
 # Would be super-duper awesome if we could define dependencies
 images=('hub' 'phibes' 'webterm')
 
 for image in "${images[@]}"; do
   echo "=============================================================="
   echo "PROCESSING ${image}....."
-  cd $image
+  pushd $image
   ./kill_clean.sh
   ./builder.sh
   ./start.sh
-  cd $PWD
+  popd
 done
