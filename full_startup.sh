7c89f2faa839b786ebfdf478b0cb993e352d2115
Sam Hart <hartsn@gmail.com>
2021-04-10 15:03:02 -0400

    hackish start to full startup on reboot

---
+++ full_startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is a rather brute force script... I really should refine it later
+# Will kill everything, rebuild, and restart
+
+PWD=$(pwd)
+PWD=${PWD%/}
+
+# Would be super-duper awesome if we could define dependencies
+images=('hub', 'phibes', 'webterm')
+
+for image in "${images[@]}"; do
+  cd $image
+  ./kill_clean.sh
+  ./builder.sh
+  ./start.sh
+  cd $PWD
+done
