2e379b4f602376eba7823b9f5c80589554a32374
Sam Hart <hartsn@gmail.com>
2021-04-14 13:55:27 -0400

    missing slash

--- phibes/start.sh
+++ phibes/start.sh
@@ -7,6 +7,6 @@ podman rm --force $instance
 podman create -t \
               --restart=always \
               --replace=true \
-              --volume=$phibes_vol:$phibes_path
+              --volume=$phibes_vol:$phibes_path \
               --name $instance $image
 podman start $instance
