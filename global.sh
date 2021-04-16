b10bb80c919dd875c61afc5737080ea2d548caf1
Sam Hart <hartsn@gmail.com>
2021-04-16 11:25:25 -0400

    pihole vols

--- global.sh
+++ global.sh
@@ -1,5 +1,13 @@
 . ~/.secrets.sh
+# Volumes
 phibes_vol=phibes-volume
 phibes_path=/opt/phibes
+
+pihole_etc_vol=pihole-etc-volume
+pihole_etc_path=/etc/pihole/
+
+pihole_dnsmasq_vol=phibes-dnsmasq-volume
+pihole_dnsmasq_path=/etc/dnsmasq.d/
+
 # Passwords
 pihole_pass=pihole-pass
--- volume_create.sh
+++ volume_create.sh
@@ -9,3 +9,18 @@ else
   echo ">>> Creating phibes volume!"
   podman volume create $phibes_vol
 fi
+
+# Pihole volumes
+if podman volume inspect $pihole_etc_vol ; then
+  echo ">>> pihole etc volume already exists!"
+else
+  echo ">>> Creating pihole etc volume!"
+  podman volume create $pihole_etc_vol
+fi
+
+if podman volume inspect $pihole_dnsmasq_vol ; then
+  echo ">>> pihole dnsmasq volume already exists!"
+else
+  echo ">>> Creating pihole dnsmasq volume!"
+  podman volume create $pihole_dnsmasq_vol
+fi
