1b4ca47fb09c1d234cce5b0f88a5b407339accf1
Sam Hart <hartsn@gmail.com>
2021-04-16 11:02:26 -0400

    more work on pihole

--- global.sh
+++ global.sh
@@ -1,2 +1,5 @@
+. ~/.secrets.sh
 phibes_vol=phibes-volume
 phibes_path=/opt/phibes
+# Passwords
+pihole_pass=pihole-pass
--- pihole/builder.sh
+++ pihole/builder.sh
@@ -8,4 +8,13 @@ buildah rmi --force $image
 
 pihole_cont=$(buildah from docker.io/pihole/pihole:latest)
 
-buildah 
+PIHOLE_ADMIN_PASS=$(\
+  podman exec phibes /usr/bin/env phibes get --password $PHIBES_PASS \
+  --path $phibes_path --item $pihole_pass | \
+  awk '/----------/{flag=1; next} /----------/{flag=0} flag')
+
+buildah run $pihole_cont pihole -a -p $PIHOLE_ADMIN_PASS
+
+buildah commit $pihole_cont $image
+
+buildah rm $python_cont
