1fe89743af442259341ed7e92dd4aba8dcaac85e
Sam Hart <hartsn@gmail.com>
2021-04-19 15:23:12 -0400

    some experiment on making this ssh-able

--- webterm/data/setup.sh
+++ webterm/data/setup.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /var/webssh2
 cd /var/webssh2
-zypper in -y npm-default git
+sed -i '/rpm.install.excludedocs = yes/c\# rpm.install.excludedocs = yes' /etc/zypp/zypp.conf
+zypper in -y npm-default git openssh-server zsh man vim
 git clone https://github.com/billchurch/webssh2.git
 cp /var/buildah-data/config.json webssh2/app/.
 cd webssh2/app/
