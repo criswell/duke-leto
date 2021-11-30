b635e4413a0645a8f947f680f93bb4e119666528
Sam Hart <hartsn@gmail.com>
2021-11-30 12:41:06 -0500

    missing run

--- one-offs/2021-11-30-timezone.sh
+++ one-offs/2021-11-30-timezone.sh
@@ -2,5 +2,5 @@
 
 # Update the timezone, ensure that the updates happen at a reasonable hour
 
-transactional-update timedatectl set-timezone America/Indianapolis
+transactional-update run timedatectl set-timezone America/Indianapolis
 
