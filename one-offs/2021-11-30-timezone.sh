19f72683a4d87501b8872cf35066c4753ea28ac3
Sam Hart <hartsn@gmail.com>
2021-11-30 12:39:34 -0500

    start one offs

---
+++ one-offs/2021-11-30-timezone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Update the timezone, ensure that the updates happen at a reasonable hour
+
+transactional-update timedatectl set-timezone America/Indianapolis
+
---
+++ one-offs/README.md
@@ -0,0 +1,6 @@
+One off scripts
+===============
+
+Little scripts that performed one-time adjustments.
+
+Shouldn't be run again, unless, you know... you have a good reason to.
